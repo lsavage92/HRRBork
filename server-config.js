9cc54e9e8201db4d57384146aab57c0285cda8b4
Luke Savage <luke.savage92@gmail.com>
2015-08-03 17:01:05 -0500

    Deleted files and updated config to reflect new project structure

--- server-config.js
+++ server-config.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
-  app.use(express.static(__dirname + '/static'));
+  app.use(express.static(__dirname + '/public'));
 });
 
 module.exports = app;
