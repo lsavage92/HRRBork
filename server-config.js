515c2601bb5280ebc8a3e36afb31397b07913d1a
Luke Savage <luke.savage92@gmail.com>
2015-08-03 14:13:48 -0500

    Attempt to fix static file serving

--- server-config.js
+++ server-config.js
@@ -1,11 +1,9 @@
 var express = require('express');
 
-var handler = require('./lib/request-handler');
-
 var app = express();
 
 app.configure(function() {
-  app.use(express.static('static'));
+  app.use(express.static(__dirname + '/static'));
 });
 
 module.exports = app;
