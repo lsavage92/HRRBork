b8f1edf60a104af8f8d41449aacb906a1c47d7f9
Luke Savage <luke.savage92@gmail.com>
2015-08-04 18:30:14 -0500

    Added assets and new rooms

--- public/lib/map.js
+++ public/lib/map.js
@@ -49,14 +49,25 @@ var rm10 = new Room(10, '/img/pub_roboschep_zoom1.png', 'Roboschep informs you t
 var rm9 = new Room(9, '/img/flower_field_octoadam.png', 'Steping outside the gate you are blinded by the beauty of an unknown creature in a remarkable landscape.',
   'Wh-where am I?', 'Maybe I do want that beer', 'Befriend the creature', 8, 12);
 
-var rm12 = new Room(12, 'img/flower_field_octoadam_zoom1.png', 'Stepping closer to the shimmering creature, he begins to speak. He informs you of a man in dire need of help and he is not too far from here. The creature says he can transport you for free and with magic.',
+var rm12 = new Room(12, '/img/flower_field_octoadam_zoom1.png', 'Stepping closer to the shimmering creature, he begins to speak. He informs you of a man in dire need of help and he is not too far from here. The creature says he can transport you for free and with magic.',
 'I definitely should have gotten that beer', 'How could I say no to... you.', 'Sorry, I have an adventure to be on', 8, 15 )
 
 //---------------------------FAIL ROOM-----------------------------------------------//
-
+var rm15 = new Room(15, '/img/trapdoor.png', 'Angered by your sudden disinterest, Octoadam banishes you to an eternity in a trap door with this guy.',
+  'Why would you ignore an octopus with a man\'s head?', 'Retry', 'Quit', 0, 2, true, './sound/Sad_Trombone.mp3');
 //--------------------------DEPTH 5--------------------------------------------------//
 
 var rm11 = new Room(11, '/img/dungeon1.png', 'After descending down the winding stairs you find yourself at the entrance to a dungeon.',
   'Can robots even drink beer?', 'Roboschep needs his beer!', 'On second thought...', 13, 14);
+//---------------------------FAIL ROOM-----------------------------------------------//
+var rm14 = new Room(14, '/img/dungeonDoor.png', 'The door is locked and you were slain by something lurking behind you.',
+  'Did you get any liquid courage?', 'Retry', 'Quit', 0, 2, true, './sound/Sad_Trombone.mp3');
 
-var rooms = [rm0, rm1, rm2, rm3, rm4, rm5, rm6, rm7, rm8, rm9, rm10, rm11, rm12];
+//--------------------------DEPTH 5--------------------------------------------------//
+var rm13 = new Room(13, '/img/dungeon1enemy.png', 'Startled by the arrival of JQuery, you are unsure how to proceed.',
+  'Suddenly, JQuery appears!', 'Ask JQuery kindly for a favor', 'Throw spaghetti at JQuery and hope for the best', 17, 16); //Should switch these rooms at some point to continue with pattern
+
+var rm16 = new Room(16, '/img/dungeon1enemyspaghetti.png', 'It worked! You threw all the spaghetti you had with you at JQuery and now it will help lift Roboscheps beer!',
+  'Success!', 'Continue', 'Eh, I\'m bored', 18, 2, true, './sound/OOT_Fanfare_Item.wav');
+
+var rooms = [rm0, rm1, rm2, rm3, rm4, rm5, rm6, rm7, rm8, rm9, rm10, rm11, rm12, rm13, rm14, rm15, rm16];
